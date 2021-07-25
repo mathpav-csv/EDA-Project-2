20c9442e9ee08025b5c7629573287e4f5b4b95e4
Vivek Mathpal <57990555+mathpav-csv@users.noreply.github.com>
2021-07-25 06:45:47 +0530

    Frsh commit

--- plot2.R
+++ plot2.R
@@ -1,10 +1,10 @@
 
-BTCity_Emissions <- summarySCC_PM25 %>% filter(fips == "24510")
+BTCity_Emissions <- NIE %>% filter(fips == "24510")
 BTfull_data <- BTCity_Emissions %>% 
                   group_by(year) %>% 
-                  summarize(Total_Emission = sum(Emissions))
+                  summarize(Total_Emission = round(sum(Emissions)/1000,2))
 
 attach(BTfull_data)
 year <- as.factor(year)
 
-plot(year,Total_Emission)
+barplot(Total_Emission,names = year,xlab = "Year",ylab = "Total Emission (in 1000's)",col = "pink", main = "Plot2")
--- plot3.R
+++ plot3.R
@@ -6,13 +6,21 @@ attach(summarySCC_PM25)
 year <- as.factor(year)
 
 
-data <- summarySCC_PM25 %>% 
+data <- NEI %>% 
         filter(fips == "24510") %>% 
         group_by(year,type) %>%
-        summarize(Total_Emission = sum(Emissions))
+        summarize(Total_Emission = round(sum(Emissions)/1000,2))
 
 glimpse(data)
 data$year <- as.factor(data$year)
 data$type <- as.factor(data$type)
 
-ggplot(data,aes(year,Total_Emission,fill = type)) + geom_bar(stat = "identity")+ facet_wrap(~type)
+z <- ggplot(data,aes(year,Total_Emission,fill = type)) + geom_bar(stat = "identity")+ facet_wrap(~type)
+
+z + theme(text = element_text(family = "serif",size = 10), 
+          panel.grid = element_blank(),
+          panel.background = element_rect(colour = "gray"))
+
+library(ggthemes)
+
+z+theme_fivethirtyeight()          
